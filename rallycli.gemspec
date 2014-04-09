39660f90145133d8c9180f784c052bedc2196b4e
Joseph Hughes <jhughes@itriagehealth.com>
2014-04-09 17:43:54 -0600

    Add coveralls

--- rallycli.gemspec
+++ rallycli.gemspec
@@ -27,4 +27,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake"
   spec.add_development_dependency "simplecov"
   spec.add_development_dependency "test_construct"
+  spec.add_development_dependency "coveralls"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 require 'bundler/setup'
 require 'timecop'
 require "test_construct/rspec_integration"
+require 'coveralls'
 require_relative 'helpers/test_helper'
 Bundler.setup
+Coveralls.wear!
 
 
 require_relative '../lib/rally/cli' # and any other gems you need
